24638da0957a8f0fd7d4a232c1ceab3402d5cb84
Zachary <zachary.j.ash@gmail.com>
2015-02-09 23:57:04 -0500

    Added Castle model

---
+++ src/Castle.cpp
@@ -0,0 +1,62 @@
+/*
+ * Castle.cpp
+ *
+ *  Created on: Feb 7, 2015
+ *      Author: Zachary Ash
+ */
+
+#include "Castle.h"
+
+void Castle::build(void*) {
+	castle.build_with_params(60 * FOOT, 40 * FOOT, 40 * FOOT, 75, 75, 75);
+	tower.build_cylinder(5 * FOOT,  4* FOOT, 30, 200, 200, 200);
+	roof.build_with_params(6 * FOOT, 10 * FOOT, 30, 102, 0, 0);
+	bigTower.build_cylinder(10 * FOOT, 20 * FOOT, 30, 255, 255, 255);
+	bigRoof.build_with_params(25 * FOOT, 30 * FOOT, 30, 255, 0, 0);
+}
+
+void Castle::render(bool) const {
+
+	//render castle building
+
+	glPushMatrix();
+	glTranslatef(0, 0, 30 * FOOT);
+	castle.render(false);
+	glPopMatrix();
+
+	//render towers
+
+	glPushMatrix();
+	glTranslatef(17 * FOOT, 17 * FOOT, 65 * FOOT);
+	tower.render(false);
+
+	glTranslatef(-34 * FOOT, 0, 0);
+	tower.render(false);
+
+	glTranslatef(0, -34 * FOOT, 0);
+	tower.render(false);
+
+	glTranslatef(34 * FOOT, 0, 0);
+	tower.render(false);
+
+	glTranslatef(0, 0, 5* FOOT);
+	roof.render(false);
+
+	glTranslatef(-34 * FOOT, 0, 0);
+	roof.render(false);
+
+	glTranslatef(0, 34 * FOOT, 0);
+	roof.render(false);
+
+	glTranslatef(34 * FOOT, 0, 0);
+	roof.render(false);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(0,0, 70 * FOOT);
+	bigTower.render(false);
+
+	glTranslatef(0, 0, 10 * FOOT);
+	bigRoof.render(false);
+	glPopMatrix();
+}
---
+++ src/Castle.h
@@ -0,0 +1,31 @@
+/*
+ * Castle.h
+ *
+ *  Created on: Feb 7, 2015
+ *      Author: Zachary Ash
+ */
+
+#ifndef CASTLE_H_
+#define CASTLE_H_
+
+#include "BufferObject.h"
+#include "Cube.h"
+#include "TCone.h"
+#include "Cone.h"
+
+#define FOOT .1
+
+class Castle : public BufferObject {
+private:
+	Cube castle;
+	TCone tower;
+	Cone roof;
+	TCone bigTower;
+	Cone bigRoof;
+
+public:
+	void build(void*);
+	void render(bool) const;
+};
+
+#endif /* CASTLE_H_ */
